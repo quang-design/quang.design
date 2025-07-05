389dddde5122f5b13143de8636d03019a889fb7f
quang-design <anhquang1806@gmail.com>
2025-07-05 12:01:32 +0700

    feat: implement image optimization with Vercel's Image Optimization API

---
+++ src/lib/components/image.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { optimize } from '$lib/utils/image';
+
+	interface Props {
+		src: string;
+		alt: string;
+	}
+
+	let { src, alt, ...props }: Props = $props();
+</script>
+
+<img class="absolute top-0 left-0 h-full w-full" srcset={optimize(src)} {alt} {...props} />
--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -7,6 +7,8 @@
 	import * as Avatar from '$lib/components/ui/avatar/index.js';
 	import { page } from '$app/state';
 	import { goto } from '$app/navigation';
+	import Image from '$lib/components/image.svelte';
+	import { optimize } from '$lib/utils/image';
 
 	let date = $state(new SvelteDate());
 
@@ -44,7 +46,7 @@
 			<Avatar.Root
 				class="size-9 border-gray-600 transition-all duration-300 ease-in-out hover:scale-105"
 			>
-				<Avatar.Image src="/avatar.jpg" alt="@quang.design" />
+				<Avatar.Image srcset={optimize('/avatar.jpg')} alt="@quang.design" />
 				<Avatar.Fallback>QN</Avatar.Fallback>
 			</Avatar.Root>
 		</a>
