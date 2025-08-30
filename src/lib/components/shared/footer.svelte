6a892bbf94cd68110462da3ba3d3e3a02a0d8414
quang-design <anhquang1806@gmail.com>
2025-08-30 10:17:32 +0700

    style: adjust footer layout and fix date visibility in navbar

--- src/lib/components/shared/footer.svelte
+++ src/lib/components/shared/footer.svelte
@@ -3,13 +3,17 @@
 </script>
 
 <footer
-	class="border-foreground/25 mt-4 grid w-full grid-cols-1 items-center gap-8 border-t-[0.5px] py-4 text-xs sm:grid-cols-3"
+	class="border-foreground/25 mt-2 grid w-full grid-cols-1 items-center gap-8 border-t-[0.5px] py-4 text-xs sm:grid-cols-3"
 >
-	<p>
-		&copy; {new Date().getFullYear()} Quang Design. All rights reserved.
-	</p>
-	<blockquote class="italic">
-		"The best person ever to exist." <a href="http://quang.urbanup.com/9858947">Urban Dictionary</a>
-	</blockquote>
+	<div class="col-span-2">
+		<blockquote class="italic">
+			"The best person ever to exist." <a href="http://quang.urbanup.com/9858947"
+				>Urban Dictionary</a
+			>
+		</blockquote>
+		<p>
+			&copy; {new Date().getFullYear()} Quang Design. All rights reserved.
+		</p>
+	</div>
 	<Subscribe />
 </footer>
--- src/lib/components/shared/navbar.svelte
+++ src/lib/components/shared/navbar.svelte
@@ -109,12 +109,11 @@
 	</div>
 
 	<div class="flex items-center gap-2">
-		<p class="hidden sm:block">{formatter.format(date)}</p>
+		<p class="sm:block">{formatter.format(date)}</p>
 		<Button variant="outline" size="icon" onclick={toggleMode} class="cursor-pointer">
 			<Sun
 				class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
 			/>
-
 			<Moon
 				class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
 			/>
